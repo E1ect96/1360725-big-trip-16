79171379628f002bd33830a4baf2ffd0a5c9c370
e1ect <e1ectt96@gmail.com>
2022-01-21 00:26:03 +0300

    3.1.4 реализует открытие и закрытие точки маршрута

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
-import {RenderPosition, renderElement} from './render.js';
+import {RenderPosition, render} from './render.js';
 import SiteMenuView from './view/site-menu-view.js';
 import FilterView from './view/filter-view.js';
 import TripInfoView from './view/trip-info-view.js';
 import SiteSortingView from './view/site-sorting-view.js';
-import PointAddFormView from './view/point-add-form-view.js';
+/*import PointAddFormView from './view/point-add-form-view.js';*/
 import PointEditFormView from './view/point-edit-form-view.js';
 import TripPointView from './view/trip-point-view.js';
 import {generateTripPoint} from './mock/trip-point.js';
@@ -22,14 +22,45 @@ const siteTripList = document.createElement('ul');
 siteTripList.classList.add('trip-events__list');
 siteTripEvents.appendChild(siteTripList);
 
-renderElement(siteMenuElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
-renderElement(siteFilterElement, new FilterView().element, RenderPosition.BEFOREEND);
-renderElement(siteTripInfo, new TripInfoView().element, RenderPosition.AFTERBEGIN);
-renderElement(siteTripEvents, new SiteSortingView().element, RenderPosition.AFTERBEGIN);
+const renderTripPoint = (TripListElement, tripPoint) => {
+  const tripPointComponent = new TripPointView(tripPoint);
+  const tripPointEditComponent = new PointEditFormView(tripPoint);
 
-for (let i = 0; i < POINT_COUNT; i++) {
-  renderElement(siteTripList, new TripPointView(tripPoints[i]).element, RenderPosition.BEFOREEND);
-}
+  const replaceCardToForm = () => {
+    TripListElement.replaceChild(tripPointEditComponent.element, tripPointComponent.element);
+  };
 
-renderElement(siteTripList, new PointAddFormView(tripPoints[tripPoints.length-1]).element, RenderPosition.BEFOREEND);
-renderElement(siteTripList, new PointEditFormView(tripPoints[0]).element, RenderPosition.AFTERBEGIN);
+  const replaceFormToCard = () => {
+    TripListElement.replaceChild(tripPointComponent.element, tripPointEditComponent.element);
+  };
+
+  tripPointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+    replaceCardToForm();
+  });
+
+  tripPointEditComponent.element.querySelector('.event__rollup-btn').addEventListener('click', (evt) => {
+    evt.preventDefault();
+    replaceFormToCard();
+  });
+
+  tripPointEditComponent.element.querySelector('form').addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    replaceFormToCard();
+  });
+
+  render(TripListElement, tripPointComponent.element, RenderPosition.BEFOREEND);
+};
+
+render(siteMenuElement, new SiteMenuView().element, RenderPosition.BEFOREEND);
+render(siteFilterElement, new FilterView().element, RenderPosition.BEFOREEND);
+render(siteTripInfo, new TripInfoView().element, RenderPosition.AFTERBEGIN);
+render(siteTripEvents, new SiteSortingView().element, RenderPosition.AFTERBEGIN);
+
+/*for (let i = 0; i < POINT_COUNT; i++) {
+  render(siteTripList, new TripPointView(tripPoints[i]).element, RenderPosition.BEFOREEND);
+}*/
+
+tripPoints.forEach((point) => renderTripPoint(siteTripList, point));
+/*
+render(siteTripList, new PointAddFormView(tripPoints[tripPoints.length-1]).element, RenderPosition.BEFOREEND);
+render(siteTripList, new PointEditFormView(tripPoints[0]).element, RenderPosition.AFTERBEGIN);*/
